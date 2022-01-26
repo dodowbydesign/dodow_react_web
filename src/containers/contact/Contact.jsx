8dc08a70c09cc7f43416130ce610d7523a078b78
DodoW <adil@dodow.design>
2022-01-26 15:39:52 +0100

    final commit 26/01 commit

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import {useForm} from "react-hook-form";
 
-
 import "./contact.css";
 import content  from "../../static";
 
 
-
-
 const Contact = () => {
 
-    const {register, HandleSubmit,errors} = useForm();
+    const {register, handleSubmit, errors} = useForm(); // can use control or register
     const onSubmit = (data) => console.log(data);
+    // console.log(register);
+    //alert("submit");
     return(
         <div id="contact">
                 <main>
@@ -20,21 +19,29 @@ const Contact = () => {
                         <p>I would love to hear from you!</p>
                         <a href="https://t.me/Dodowbydesign"><i className="fab fa-telegram"></i></a>
                     </section>
-                    <form onSubmit={onSubmit}>
+                    <form onSubmit={()=>handleSubmit(onSubmit)}>
                         {content.inputs.map((input,key)=>{
                             return(
                                 <div key={key}>
                                     <label htmlFor={input.label}>{input.label}</label>
-                                    <input name={input.name} id={input.name} type={input.type} placeholder={input.label} />
+                                    <input name={input.name}
+                                           id={input.name}
+                                           type={input.type}
+                                           placeholder={input.label}
+                                    />
                                 </div>
-
                             );
                         })}
                         {content.textarea.map((textarea,key)=>{
                             return(
                                 <div key={key}>
                                     <label htmlFor={textarea.label}>{textarea.label}</label>
-                                    <textarea name={textarea.name} id={textarea.name} type={textarea.type} placeholder={textarea.label} maxLength={textarea.maxLength} placeholder={textarea.placeholder}></textarea>
+                                    <textarea name={textarea.name}
+                                              id={textarea.name}
+                                              maxLength={textarea.maxLength}
+                                              placeholder={textarea.placeholder}
+                                    >
+                                    </textarea>
                                 </div>
 
                             );
